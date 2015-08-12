0ff2fdb99bf4fa81c0d996b9b66ee927fc8d8e02
Anthony Brummett <brummett@gmail.com>
2015-08-12 15:29:49 -0500

    The game now waits for the prescribed number of unique players to connect

--- lib/SetupController.pm
+++ lib/SetupController.pm
@@ -1,18 +1,18 @@
 use v6;
 
 use WebController;
+use Player;
 
 class SetupController is WebController {
     has Int $.num_players = 2;
-    has Str %!player_names;
+    has Int %!player_names;  # values are which player number that name is
     has Int $!current_num_players = 0;
-    has Bool $!started = False;
 
     method new {
         my $self = callsame;
         $self.get: '/' => sub { $self.enter_name };
-        $self.post: '/add_player/:player_num' => sub ($player_num) { $self.add_player($player_num) };
-        $self.get: '/waiting_for_players' => sub { $self.waiting_for_players };
+        $self.post: '/add_player/:player_num' => sub ($n) { $self.add_player($n) };
+        $self.get: '/waiting/:player_num' => sub ($n) { $self.waiting($n) };
         return $self;
     }
 
@@ -25,19 +25,41 @@ class SetupController is WebController {
     method add_player(Cool $player_num) {
         my $player_name = self.request.params<player_name>;
         say "Adding player number $player_num named $player_name";
-        if (%!player_names{$player_name}) {
+        if (%!player_names{$player_name}:exists) {
             my @tmpl_args = { player_number => $player_num, dup_name => $player_name};
             self.template: 'enter_name.tt', @tmpl_args;
-        } elsif ($!current_num_players >= $.num_players) {
+        } elsif (self.ready_to_start) {
             self.start_game();
         } else {
-            my @tmpl_args = { num_players => $!num_players };
+            %!player_names{$player_name} = $player_num.Int;
+            self.status(303);
+            self.header('Content-Type', 'text/html');
+            self.header('Location', "/waiting/$player_num");
+        }
+    }
+
+    method ready_to_start() returns Bool {
+        return %!player_names.elems >= $.num_players;
+    }
+
+    method waiting($player_num) {
+        if (self.ready_to_start) {
+            self.start_game();
+        } else {
+            my @tmpl_args = { num_players => $!current_num_players,
+                              waiting_on_players => $.num_players - $!current_num_players,
+                            };
             self.template('waiting_for_players.tt', @tmpl_args);
         }
     }
 
     method start_game() {
         say "Starting game!";
+        my Player @players;
+        for %!player_names.kv -> $name, $player_num {
+            @players[$player_num] = Player.new(name => $name);
+        }
+        self.return(@players);
     }
 }
 
